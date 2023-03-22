37ff4ca5d00e3a87fe66ba9d33cf66e0aac0c21e
Daniel Hanhart <daniel.hanhart@gmail.com>
2023-03-22 10:00:48 +0100

    22.03.2023

--- scripts/create_count_matrices.R
+++ scripts/create_count_matrices.R
@@ -1,7 +1,9 @@
-#### Bone HB data ##############################################################
+#### Bone data ##############################################################
 library(tidyverse)
 library(tximport)
 library(xlsx)
+
+
 
 if(FALSE){
 P <- "/media/triller/TOSHIBA\ EXT/Data/RNAseq/nonunion/"
@@ -10,19 +12,36 @@ bone_metadata <- read.table("./local/bone_metadata.tsv", sep = "\t", header = TR
 ## only HB samples from 2D
 bone_metadata <- bone_metadata %>% dplyr::filter(Condition == "2D")
 
+
 ## samples used (the samples Andre_S1 and Andre_S3 were swaped, for that reasen S3 is read in for S1 abd S1 for S3!)
 samples_read_in <- bone_metadata$read_in
-samples <- bone_metadata$Sample_file_name
+samples <- bone_metadata$GS_Code
 
-## tximport data (read data directly from salmon output, quant.sf files)
+
+
+ ## tximport data (read data directly from salmon output, quant.sf files)
 fs <- file.path(paste0(P, "GS_rnaInput_", samples_read_in , ".csv", "/star_salmon/", samples_read_in, "/quant.sf"))
 names(fs) <- samples
 ## tx2gene always the same for the same reference genome
 tx2gene <- read.table(paste0(P, "GS_rnaInput_", samples_read_in[1] , ".csv", "/star_salmon/salmon_tx2gene.tsv"))
 tx_data <- tximport(fs, type = "salmon", tx2gene = tx2gene, countsFromAbundance = "lengthScaledTPM")
-rownames(bone_metadata) <- bone_metadata$Sample_file_name
+saveRDS(tx_data,"./local/bone_tximport_lengthScaledTPM.rds")
+write.table(tx_data[["abundance"]], "./local/bone_tximport_lengthScaledTPM.tsv", quote = TRUE, sep = "\t", col.names = TRUE, row.names = TRUE )
 }
 
-saveRDS("./local/bone_tximport.rds")
+#### prostate cancer data (rapalink) ###########################################
 
-tx_data$countsFromAbundance
+if(FALSE){
+P <- "/media/triller/TOSHIBA\ EXT/Data/RNAseq/rapalink/"
+
+rapalink_metadata <- read.table("./local/rapalink_metadata.txt", sep = "\t", header = TRUE)
+
+samples <- rapalink_metadata$GenomeScanID
+
+fs <- file.path(paste0(P, "GS_", samples, "/star_salmon/", samples, "/quant.sf"))
+names(fs) <- samples
+tx2gene <- read.table(paste0(P, "GS_", samples[1] , "/star_salmon/salmon_tx2gene.tsv"))
+tx_data <- tximport(fs, type = "salmon", tx2gene = tx2gene, countsFromAbundance = "lengthScaledTPM")
+saveRDS(tx_data,"./local/rapalink_tximport_lengthScaledTPM.rds")
+write.table(tx_data[["abundance"]], "./local/rapalink_tximport_lengthScaledTPM.tsv", quote = TRUE, sep = "\t", col.names = TRUE, row.names = TRUE )
+}
